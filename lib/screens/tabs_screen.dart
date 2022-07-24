5d7382f48ef96e365970cf8b6a3b913a96640a4a
AllanCicceri <allan.cicceri@gmail.com>
2022-07-24 17:02:30 -0300

    add: bottom tab controller

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,34 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:meal/screens/categories_screen.dart';
 import 'package:meal/screens/favorites_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
   const TabsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+
+  List<Widget> _screens = [
+    CategoriesScreen(),
+    FavoritesScreen(),
+  ];
+
+  List<String> _titles = ['Categories', 'Favorites'];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: AppBar(
-          title: const Center(child: Text('Let' 's cook!')),
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favorites',
-              ),
-            ],
-          ),
+        appBar:
+            AppBar(title: Center(child: Text(_titles[_selectedScreenIndex]))),
+        body: _screens[_selectedScreenIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectScreen,
+          currentIndex: _selectedScreenIndex,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.category), label: 'Categories'),
+            BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorites'),
+          ],
         ),
-        body: const TabBarView(children: [
-          CategoriesScreen(),
-          FavoritesScreen(),
-        ]),
       ),
     );
   }
 }
+
+
+
+// tab na appbar:
+// bottom: const TabBar(
+          //   tabs: [
+          //     Tab(
+          //       icon: Icon(Icons.category),
+          //       text: 'Categories',
+          //     ),
+          //     Tab(
+          //       icon: Icon(Icons.star),
+          //       text: 'Favorites',
+          //     ),
+          //   ],
+          // ),
